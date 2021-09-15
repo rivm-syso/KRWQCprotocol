7a21bc6ab0cabfc82ef4cbaf476728901f17eb80
Job Spijker <job.spijker@rivm.nl>
2021-09-15 10:08:02 +0200

    Some initial dataset documentation added
    See #1

---
+++ R/datasets.R
@@ -0,0 +1,108 @@
+#' Veldgegevens grondwater monitoring
+#'
+#' Test dataset op basis van het LMG met daarin veldgegevens. Deze
+#' dataset kan gebruikt worden voor het testen van de functies uit dit
+#' pakket. 
+#'
+#' @format een data.frame met 14 kolommen en 2025 rijen.
+#'
+#'
+"veld"
+
+#' Parameter gegevens
+#'
+#' Test dataset op basis van het LMG met daarin gemeten parameters. Deze
+#' dataset kan gebruikt worden voor het testen van de functies uit dit
+#' pakket. 
+#'
+#' @format een data.frame met 11 kolommen en 61 rijen.
+#'
+"parameter"
+
+#' Grondwatermetingen
+#'
+#' Test dataset op basis van het LMG met daarin gemeten
+#' grondwaterconcentraties Deze
+#' dataset kan gebruikt worden voor het testen van de functies uit dit
+#' pakket. 
+#'
+#' @format een data.frame met 11 kolommen en 211792 rijen.
+#'
+"metingen"
+
+#' Put gegevens
+#'
+#' Test dataset op basis van het LMG met daarin gegevens over de
+#' putten. Deze
+#' dataset kan gebruikt worden voor het testen van de functies uit dit
+#' pakket. 
+#'
+#' @format een data.frame met 8 kolommen en 340 rijen.
+#'
+"put"
+
+#' Filter gegevens
+#'
+#' Test dataset op basis van het LMG met daarin informatie over de
+#' filters. Deze
+#' dataset kan gebruikt worden voor het testen van de functies uit dit
+#' pakket. 
+#'
+#' @format een data.frame met 8 kolommen en 340 rijen.
+#'
+"filter"
+
+#' BRO opzoektabellen
+#'
+#' Opzoektabellen met informatie uit de BRO
+#' Deze tabel bevat de geregistreerde bemonsteringsapparaten
+#'
+#'
+
+"BRO_bemonsteringsapparaat"
+
+
+#' BRO opzoektabellen
+#'
+#' Opzoektabellen met informatie uit de BRO
+#' Deze tabel bevat de geregistreerde bemonsteringsprocedures
+#'
+#'
+#'
+"BRO_bemonsteringsprocedure"
+
+#' BRO opzoektabellen
+#'
+#' Opzoektabellen met informatie uit de BRO
+#' Deze tabel bevat de geregistreerde beoordelingsprocedure
+#'
+#'
+#'
+"BRO_beoordelingsprocedure"
+
+#' BRO opzoektabellen
+#'
+#' Opzoektabellen met informatie uit de BRO
+#' Deze tabel bevat de geregistreerde parameters
+#'
+#'
+#'
+"BRO_parameterlijst"
+
+#' BRO opzoektabellen
+#'
+#' Opzoektabellen met informatie uit de BRO
+#' Deze tabel bevat de geregistreerde waardebepalingsprocedures
+#'
+#'
+"BRO_waardebepalingsprocedure"
+
+#' BRO opzoektabellen
+#'
+#' Opzoektabellen met informatie uit de BRO
+#' Deze tabel bevat de geregistreerde waardebepalingstechnieken
+#'
+#'
+#'
+"BRO_waardebepalingstechniek"
+
