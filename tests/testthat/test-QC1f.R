fdf1edc43e7525daaac3ac8c98318925b5910c95
Job Spijker <job.spijker@rivm.nl>
2022-02-08 17:34:17 +0100

    Test QC1f toegevoegd
    Maar functie blijkt niet te werken, negatieve waarden worden niet
    herkend.
    
    Zie #26

---
+++ tests/testthat/test-QC1f.R
@@ -0,0 +1,39 @@
+test_that("QC1f T1 standaard waarden", {
+
+  data(metingen)
+  d <- metingen
+  
+  x <- QC1f(d_metingen = d)
+  
+  # test if attributes exist
+  expect_true(qcout_attrexists(x))
+  x_attr <- attr(x, "qcout")
+  expect_false(is.null(x_attr[["QC1f"]]))
+  expect_true(is.list(x_attr[["QC1f"]][["resultaat"]]))
+
+
+  ids <- x_attr[["QC1f"]][["oordeel"]][["verdacht"]]
+  qcids <- metingen$qcid
+  v1 <- intersect(ids, qcids)
+  expect_true(length(v1) == 0)
+
+  expect_true(nrow(metingen) == nrow(x))
+
+
+})
+
+
+test_that("QC1f T2 negatieve waarden", {
+
+              data(metingen)
+              d <- metingen
+              d$waarde[1] <- -1
+
+              x <- QC1f(d_metingen = d)
+
+              # test if attributes exist
+              ids <- x_attr[["QC1f"]][["oordeel"]][["verdacht"]]
+              qcids <- metingen$qcid
+              v1 <- intersect(ids, qcids)
+              expect_true(length(v1) == 1)
+})
